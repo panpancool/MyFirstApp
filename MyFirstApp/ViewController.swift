79bd22becc4efdd82fde81f97118dca23b24cdbb
潘柏全 <panbaiquan@panbaiquande-macbook-pro.local>
2018-07-17 19:38:05 +0800

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by 潘柏全 on 2018/7/17.
+//  Copyright © 2018年 Pan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
